2a19f127fcf19a8c3adfbff7ffc3c2b7f29e1bd8
kuangle <kuangle@kingsoft.com>
2020-08-21 15:07:56 +0800

    add: 增加服务多节点注册注销功能

--- registercenter/etcdregister.go
+++ registercenter/etcdregister.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"fmt"
 	"go.etcd.io/etcd/clientv3"
-	"go.etcd.io/etcd/etcdserver/api/v3rpc/rpctypes"
 	"log"
+	"strings"
 )
 
 type etcdRegisterImpl struct {
@@ -30,21 +30,21 @@ func (etcd *etcdRegisterImpl) Register(info ServiceDescInfo) error {
 		return err
 	}
 	// should get first, if not exist, set it
-	_, err = client.Get(context.Background(), info.ServiceName)
+	kv, err := client.Get(context.Background(), info.ServiceName)
+	log.Printf("%v", kv)
 	serviceValue := fmt.Sprintf("%s:%d", info.Host, info.Port)
 	if err != nil {
-		if err == rpctypes.ErrKeyNotFound {
-			if _, err := client.Put(context.TODO(), info.ServiceName, serviceValue, clientv3.WithLease(resp.ID)); err != nil {
-				log.Printf("etcd: set service '%s' with ttl to etcd3 failed: %s", info.ServiceName, err.Error())
-			}
-		} else {
-			log.Printf("etcd: service '%s' connect to etcd3 failed: %s", info.ServiceName, err.Error())
-			return err
+		log.Printf("etcd: service '%s' connect to etcd3 failed: %s", info.ServiceName, err.Error())
+		return err
+	}
+	if kv.Kvs == nil {
+		if _, err := client.Put(context.TODO(), info.ServiceName, serviceValue, clientv3.WithLease(resp.ID)); err != nil {
+			log.Printf("etcd: set service '%s' with ttl to etcd3 failed: %s", info.ServiceName, err.Error())
 		}
 	} else {
 		// refresh set to true for not notifying the watcher
-		if _, err := client.Put(context.Background(), info.ServiceName, serviceValue, clientv3.WithLease(resp.ID)); err != nil {
-			log.Printf("etcd: refresh service '%s' with ttl to etcd3 failed: %s", info.ServiceName, err.Error())
+		err := etcd.update(info.ServiceName, serviceValue, client, resp)
+		if err != nil {
 			return err
 		}
 	}
@@ -58,10 +58,73 @@ func (etcd *etcdRegisterImpl) UnRegister(info ServiceDescInfo) error {
 	if err != nil {
 		return err
 	}
-	if _, err := client.Delete(context.Background(), info.ServiceName); err != nil {
-		log.Printf("etcd: deregister '%s' failed: %s", info.ServiceName, err.Error())
-	} else {
-		log.Printf("etcd: deregister '%s' ok.", info.ServiceName)
+	serviceValue := fmt.Sprintf("%s:%d", info.Host, info.Port)
+	resp, err := client.Grant(context.TODO(), int64(info.IntervalTime))
+	if err != nil {
+		log.Println("创建租约失败:", err)
+		return err
+	}
+	err = etcd.removeFormList(info.ServiceName, serviceValue, client, resp)
+	if err != nil {
+		return err
 	}
 	return err
+}
+
+func (etcd *etcdRegisterImpl) update(k, v string, client *clientv3.Client, LeaseResp *clientv3.LeaseGrantResponse) (err error) {
+	// TODO 此处需要注意并发事务问题,可以采用watch 乐观锁 + channel 抢占方式
+	resp, err := client.Get(context.Background(), k)
+	if err != nil {
+		log.Printf("etcd get service fail %s", err.Error())
+		return
+	}
+	if len(resp.Kvs) != 1 {
+		log.Printf("etcd update service fail")
+		return
+	}
+	value := string(resp.Kvs[0].Value)
+	newValue := fmt.Sprintf("%s,%s", value, v)
+	_, err = client.Put(context.Background(), k, newValue, clientv3.WithLease(LeaseResp.ID))
+	if err != nil {
+		log.Printf("etcd: refresh service '%s' with ttl to etcd3 failed: %s", k, err.Error())
+		return
+	}
+	return
+}
+
+func (etcd *etcdRegisterImpl) removeFormList(k, v string, client *clientv3.Client, LeaseResp *clientv3.LeaseGrantResponse) (err error) {
+	resp, err := client.Get(context.Background(), k)
+	if err != nil {
+		log.Printf("etcd get service fail %s", err.Error())
+		return
+	}
+	if len(resp.Kvs) != 1 {
+		log.Printf("etcd remove fail")
+		return
+	}
+	values := string(resp.Kvs[0].Value)
+	vArray := strings.Split(values, ",")
+	newValues := ""
+	for _, e := range vArray {
+		if newValues != "" && e != v {
+			newValues = fmt.Sprintf("%s,", newValues)
+		}
+		if e != v {
+			newValues = fmt.Sprintf("%s%s", newValues, e)
+		}
+	}
+	if newValues == "" {
+		_, err = client.Delete(context.Background(), k)
+		if err != nil {
+			log.Printf("etcd delete empty kvs fail[%s]", err.Error())
+			return
+		}
+		return
+	}
+	_, err = client.Put(context.Background(), k, newValues, clientv3.WithLease(LeaseResp.ID))
+	if err != nil {
+		log.Printf("etcd update service[%s] to new service[%s] fail.", values, newValues)
+		return
+	}
+	return
 }
--- test/server/hello_register_test.go
+++ test/server/hello_register_test.go
@@ -71,13 +71,42 @@ func TestStartClient2(t *testing.T) {
 	}
 }
 
+func TestStartClient3(t *testing.T) {
+	lis, err := net.Listen("tcp", "127.0.0.1:8092")
+	if err != nil {
+		t.Errorf("failed to listen: %v", err)
+		return
+	}
+	etcdRegister := registercenter.NewEtcdRegisterImpl("8.210.188.38:2379")
+
+	go func() {
+		err := etcdRegister.Register(registercenter.ServiceDescInfo{
+			ServiceName:  "HelloService",
+			Host:         "127.0.0.1",
+			Port:         8092,
+			IntervalTime: time.Duration(10000),
+		})
+		if err != nil {
+			t.Errorf("failed to register: %v", err)
+		}
+		time.Sleep(time.Second * 1)
+	}()
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterHelloServer(grpcServer, server.HelloService)
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		t.Errorf("failed to serve:%v", err)
+		return
+	}
+}
 func TestUnRegisterServer(t *testing.T) {
 	etcdRegister := registercenter.NewEtcdRegisterImpl("8.210.188.38:2379")
 	err := etcdRegister.UnRegister(registercenter.ServiceDescInfo{
 		ServiceName:  "HelloService",
 		Host:         "127.0.0.1",
 		Port:         8090,
-		IntervalTime: time.Duration(1),
+		IntervalTime: time.Duration(10000),
 	})
 	if err != nil {
 		log.Printf("fail to UnRegister%v", err)
